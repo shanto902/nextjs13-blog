0ddcacd3b17643c338c9b64d76b257bb297d7331
Ashik Ali Shanto <shanto902@gmail.com>
2023-10-29 12:54:19 +0600

    OMG 4

--- utils/shimmer.ts
+++ utils/shimmer.ts
@@ -2,15 +2,17 @@ export const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#888" offset="20%" />
-      <stop stop-color="#444" offset="50%" />
-      <stop stop-color="#888" offset="70%" />
+      <stop stop-color="rgba(136, 136, 136, 0.2)" offset="20%" />
+      <stop stop-color="rgba(68, 68, 68, 0.2)" offset="50%" />
+      <stop stop-color="rgba(136, 136, 136, 0.2)" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#888" />
+  <rect width="${w}" height="${h}" fill="none" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
+  <animate attributeName="x" from="-${w}" to="${w}" dur="5s" repeatCount="indefinite" />
+</svg>
+
+`;
 
 export const toBase64 = (str: string) =>
   typeof window === "undefined"
